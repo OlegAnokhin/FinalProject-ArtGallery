b3d43cecd1db8a91069a9c56640fc9650607e13e
Oleg Anokhin <oleg_anokhin@abv.bg>
2023-07-13 20:06:19 +0300

    Add custom error page view

--- ArtGallery.Web/Program.cs
+++ ArtGallery.Web/Program.cs
@@ -45,13 +45,13 @@ namespace ArtGallery.Web
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
-                app.UseMigrationsEndPoint();
                 app.UseDeveloperExceptionPage();
+                app.UseMigrationsEndPoint();
             }
             else
             {
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseExceptionHandler("/Home/Error/500");
+                app.UseStatusCodePagesWithRedirects("/Home/Error?statusCode={0}");
                 app.UseHsts();
             }
 
