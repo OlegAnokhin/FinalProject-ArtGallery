3b2a789cb7857df8fa5e9a0d0cd432e9bb672178
Oleg Anokhin <oleg_anokhin@abv.bg>
2023-08-11 17:12:25 +0300

    Add more tests for PictureController

--- ArtGallery.Services.Tests/Controllers/PictureControllerTests.cs
+++ ArtGallery.Services.Tests/Controllers/PictureControllerTests.cs
@@ -4,10 +4,13 @@
     using Microsoft.AspNetCore.Http;
     using Microsoft.AspNetCore.Mvc;
     using Microsoft.Extensions.Logging;
+    using Microsoft.AspNetCore.Mvc.ViewFeatures;
     using Moq;
+    using Data.Models.Picture;
     using Data.Interfaces;
-    using Web.Controllers;
     using Mocks.Models;
+    using Web.Controllers;
+    using Web.ViewModels.Picture;
 
     [TestFixture]
     public class PictureControllerTests
@@ -47,34 +50,66 @@
             };
         }
 
-        //[Test]
-        //public void AddGetMethodShouldReturnsView()
-        //{
-        //    var categories = new List<PictureSelectCategoryModel>
-        //    {
-        //        new PictureSelectCategoryModel
-        //        {
-        //            Id = 1,
-        //            Name = "Name"
-        //        }
-        //    };
-        //    //categoryServiceMock.Setup(x => x.AllCategoriesAsync())
-        //    //    .Returns(Task.FromResult(new AddAndEditPictureViewModel()
-        //    //    {
-        //    //        Name = "Name",
-        //    //        Size = "Size",
-        //    //        Material = "Material",
-        //    //        ImageAddress = "ImageAddress",
-        //    //        ImageBase = "ImageBase",
-        //    //        CategoryId = 1,
-        //    //        Description = "Description",
-        //    //        CreatedOn = DateTime.UtcNow
-        //    //    }));
-        //    var result = this.controller.Add();
+        [Test]
+        public async Task AllMethodReturnsCorrectViewAndModel()
+        {
+            var pictureList = new List<AllPictureViewModel>
+            {
+                new AllPictureViewModel { Id = 1, Name = "Picture 1", ImageAddress = "url1.jpg" },
+                new AllPictureViewModel() { Id = 2, Name = "Picture 2", ImageAddress = "url2.jpg" }
+            };
+            var queryModel = new AllPictureQueryModel();
+            var serviceModel = new AllPicturesFilteredAndPagedServiceModel
+            {
+                Pictures = pictureList,
+                TotalPicturesCount = 10
+            };
 
-        //    Assert.That(result, Is.Not.Null);
-        //    Assert.That(result, Is.TypeOf<ViewResult>());
-        //}
+            pictureServiceMock.Setup(x => x.AllAsync(queryModel))
+                .ReturnsAsync(serviceModel);
+
+            categoryServiceMock.Setup(x => x.AllCategoryNamesAsync())
+                .ReturnsAsync(new List<string>());
+
+            var result = await controller.All(queryModel) as ViewResult;
+
+            Assert.IsNotNull(result);
+            Assert.That(result, Is.TypeOf<ViewResult>());
+            Assert.IsInstanceOf<AllPictureQueryModel>(result.Model);
+        }
+
+        [Test]
+        public async Task AddGetMethodShouldRedirectToActionWhenUserNotAdmin()
+        {
+            ClaimsPrincipal user = new ClaimsPrincipal(new ClaimsIdentity(new Claim[]
+            {
+                new Claim(ClaimTypes.Name, "name"),
+                new Claim(ClaimTypes.NameIdentifier, "nameIdentifier"),
+                new Claim(ClaimTypes.Role, "role")
+            }, "mock"));
+
+            controller.ControllerContext = new ControllerContext()
+            {
+                HttpContext = new DefaultHttpContext()
+                {
+                    User = user
+                }
+            };
+
+            var result = await this.controller.Add();
+
+            Assert.That(result, Is.Not.Null);
+            Assert.That(result, Is.TypeOf<RedirectToActionResult>());
+        }
+
+        [Test]
+        public async Task AddGetMethodShouldReturnsView()
+        {
+            var result = await this.controller.Add();
+
+            Assert.That(result, Is.Not.Null);
+            Assert.That(result, Is.TypeOf<ViewResult>());
+        }
 
         [Test]
         public async Task AddPostMethodShouldReturnsRedirectToActionWhenModelStateIsValid()
@@ -88,7 +123,7 @@
         }
 
         [Test]
-        public async Task AddPostMethodShouldReturnsRedirectToActionWhenModelStateIsInvalid()
+        public async Task AddPostMethodShouldReturnsViewResultModelStateIsInvalid()
         {
             controller.ModelState.AddModelError("test", "test");
             var result = await this.controller.Add(AddAndEditPictureViewModelMock.Instance());
@@ -97,5 +132,137 @@
             Assert.That(result, Is.TypeOf<ViewResult>());
         }
 
+        [Test]
+        public async Task DetailsMethodShouldReturnsViewResultWhenPictureExist()
+        {
+            pictureServiceMock.Setup(x => x.ExistByIdAsync(It.IsAny<int>()))
+                .Returns(Task.FromResult(true));
+
+            pictureServiceMock.Setup(x => x.GetDetailsByIdAsync(It.IsAny<int>()))
+                .Returns(Task.FromResult(new DetailsPictureViewModel
+                {
+                    Id = 1,
+                    Name = "Name",
+                    Size = "Size",
+                    Material = "Material",
+                    ImageAddress = "ImageAddress",
+                    ImageBase = "ImageBase",
+                    Description = "Description"
+                }));
+            var result = await this.controller.Details(1);
+
+            Assert.That(result, Is.Not.Null);
+            Assert.That(result, Is.TypeOf<ViewResult>());
+        }
+
+        [Test]
+        public async Task DetailsMethodShouldReturnsRedirectToActionWhenPictureNotExist()
+        {
+            var tempData = new TempDataDictionary(new DefaultHttpContext(), Mock.Of<ITempDataProvider>());
+            tempData["ErrorMessage"] = "test";
+            controller.TempData = tempData;
+
+            var result = await this.controller.Details(1);
+
+            Assert.That(result, Is.Not.Null);
+            Assert.That(result, Is.TypeOf<RedirectToActionResult>());
+        }
+
+        [Test]
+        public async Task EditGetMethodShouldReturnsViewResultWhenPictureExist()
+        {
+            pictureServiceMock.Setup(x => x.ExistByIdAsync(It.IsAny<int>()))
+                .Returns(Task.FromResult(true));
+
+            pictureServiceMock.Setup(x => x.GetPictureForEditAsync(It.IsAny<int>()))
+                .Returns(Task.FromResult(new AddAndEditPictureViewModel()
+                {
+                    Name = "Name",
+                    Size = "Size",
+                    Material = "Material",
+                    ImageAddress = "ImageAddress",
+                    ImageBase = "ImageBase",
+                    CategoryId = 1,
+                    Description = "Description"
+                }));
+            var result = await this.controller.Edit(1);
+
+            Assert.That(result, Is.Not.Null);
+            Assert.That(result, Is.TypeOf<ViewResult>());
+        }
+
+        [Test]
+        public async Task EditGetMethodShouldReturnsRedirectToActionWhenPictureNotExist()
+        {
+            var tempData = new TempDataDictionary(new DefaultHttpContext(), Mock.Of<ITempDataProvider>());
+            tempData["ErrorMessage"] = "test";
+            controller.TempData = tempData;
+
+            var result = await this.controller.Edit(1);
+
+            Assert.That(result, Is.Not.Null);
+            Assert.That(result, Is.TypeOf<RedirectToActionResult>());
+        }
+
+        [Test]
+        public async Task EditPostMethodShouldReturnsRedirectToActionWhenPictureExist()
+        {
+            pictureServiceMock.Setup(x => x.ExistByIdAsync(It.IsAny<int>()))
+                .Returns(Task.FromResult(true));
+
+            var result = await this.controller.Edit(1, AddAndEditPictureViewModelMock.Instance());
+
+            Assert.That(result, Is.Not.Null);
+            Assert.That(result, Is.TypeOf<RedirectToActionResult>());
+        }
+
+        [Test]
+        public async Task EditPostMethodShouldReturnsViewResultWhenModelStateIsInvalid()
+        {
+            controller.ModelState.AddModelError("test", "test");
+            var result = await this.controller.Edit(1, AddAndEditPictureViewModelMock.Instance());
+
+            Assert.That(result, Is.Not.Null);
+            Assert.That(result, Is.TypeOf<ViewResult>());
+        }
+
+        [Test]
+        public async Task EditPostMethodShouldReturnsRedirectToActionWhenArtEventNotExist()
+        {
+            var tempData = new TempDataDictionary(new DefaultHttpContext(), Mock.Of<ITempDataProvider>());
+            tempData["ErrorMessage"] = "test";
+            controller.TempData = tempData;
+
+            var result = await this.controller.Edit(1, AddAndEditPictureViewModelMock.Instance());
+
+
+            Assert.That(result, Is.Not.Null);
+            Assert.That(result, Is.TypeOf<RedirectToActionResult>());
+        }
+
+        [Test]
+        public async Task DeleteMethodShouldReturnsRedirectToActionWhenPictureExist()
+        {
+            pictureServiceMock.Setup(x => x.ExistByIdAsync(It.IsAny<int>()))
+                .Returns(Task.FromResult(true));
+
+            var result = await this.controller.Delete(1);
+
+            Assert.That(result, Is.Not.Null);
+            Assert.That(result, Is.TypeOf<RedirectToActionResult>());
+        }
+
+        [Test]
+        public async Task DeleteMethodShouldReturnsRedirectToActionWhenPictureNotExist()
+        {
+            var tempData = new TempDataDictionary(new DefaultHttpContext(), Mock.Of<ITempDataProvider>());
+            tempData["ErrorMessage"] = "test";
+            controller.TempData = tempData;
+
+            var result = await this.controller.Delete(1);
+
+            Assert.That(result, Is.Not.Null);
+            Assert.That(result, Is.TypeOf<RedirectToActionResult>());
+        }
     }
 }
