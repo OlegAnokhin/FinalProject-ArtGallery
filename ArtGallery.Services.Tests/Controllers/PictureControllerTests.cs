8f97fefd5494f5f01ed9203ff6c11e29f5c46d5d
Oleg Anokhin <oleg_anokhin@abv.bg>
2023-08-11 12:14:56 +0300

    Add new tests

---
+++ ArtGallery.Services.Tests/Controllers/PictureControllerTests.cs
@@ -0,0 +1,101 @@
+﻿namespace ArtGallery.Services.Tests.Controllers
+{
+    using System.Security.Claims;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Extensions.Logging;
+    using Moq;
+    using Data.Interfaces;
+    using Web.Controllers;
+    using Mocks.Models;
+
+    [TestFixture]
+    public class PictureControllerTests
+    {
+        private Mock<IPictureService> pictureServiceMock;
+        private Mock<ICategoryService> categoryServiceMock;
+        private Mock<ICommentService> commentServiceMock;
+        private Mock<ILogger<PictureController>> loggerMock;
+        private PictureController controller;
+
+        private const string name = "admin@ArtGallery.bg";
+        private const string nameIdentifier = "d53a80c3-5fd9-4451-a381-f40d2f50ec08";
+        private const string role = "Administrator";
+
+        [SetUp]
+        public async Task SetUp()
+        {
+            pictureServiceMock = new Mock<IPictureService>();
+            categoryServiceMock = new Mock<ICategoryService>();
+            commentServiceMock = new Mock<ICommentService>();
+            loggerMock = new Mock<ILogger<PictureController>>();
+            this.controller = new PictureController(pictureServiceMock.Object, categoryServiceMock.Object, commentServiceMock.Object, loggerMock.Object);
+
+            ClaimsPrincipal user = new ClaimsPrincipal(new ClaimsIdentity(new Claim[]
+            {
+                new Claim(ClaimTypes.Name, name),
+                new Claim(ClaimTypes.NameIdentifier, nameIdentifier),
+                new Claim(ClaimTypes.Role, role)
+            }, "mock"));
+
+            controller.ControllerContext = new ControllerContext()
+            {
+                HttpContext = new DefaultHttpContext()
+                {
+                    User = user
+                }
+            };
+        }
+
+        //[Test]
+        //public void AddGetMethodShouldReturnsView()
+        //{
+        //    var categories = new List<PictureSelectCategoryModel>
+        //    {
+        //        new PictureSelectCategoryModel
+        //        {
+        //            Id = 1,
+        //            Name = "Name"
+        //        }
+        //    };
+        //    //categoryServiceMock.Setup(x => x.AllCategoriesAsync())
+        //    //    .Returns(Task.FromResult(new AddAndEditPictureViewModel()
+        //    //    {
+        //    //        Name = "Name",
+        //    //        Size = "Size",
+        //    //        Material = "Material",
+        //    //        ImageAddress = "ImageAddress",
+        //    //        ImageBase = "ImageBase",
+        //    //        CategoryId = 1,
+        //    //        Description = "Description",
+        //    //        CreatedOn = DateTime.UtcNow
+        //    //    }));
+        //    var result = this.controller.Add();
+
+        //    Assert.That(result, Is.Not.Null);
+        //    Assert.That(result, Is.TypeOf<ViewResult>());
+        //}
+
+        [Test]
+        public async Task AddPostMethodShouldReturnsRedirectToActionWhenModelStateIsValid()
+        {
+            categoryServiceMock.Setup(x => x.ExistsByIdAsync(It.IsAny<int>()))
+                .Returns(Task.FromResult(true));
+            var result = await this.controller.Add(AddAndEditPictureViewModelMock.Instance());
+
+            Assert.That(result, Is.Not.Null);
+            Assert.That(result, Is.TypeOf<RedirectToActionResult>());
+        }
+
+        [Test]
+        public async Task AddPostMethodShouldReturnsRedirectToActionWhenModelStateIsInvalid()
+        {
+            controller.ModelState.AddModelError("test", "test");
+            var result = await this.controller.Add(AddAndEditPictureViewModelMock.Instance());
+
+            Assert.That(result, Is.Not.Null);
+            Assert.That(result, Is.TypeOf<ViewResult>());
+        }
+
+    }
+}
---
+++ ArtGallery.Services.Tests/Mocks/Models/AddAndEditPictureViewModelMock.cs
@@ -0,0 +1,22 @@
+﻿namespace ArtGallery.Services.Tests.Mocks.Models
+{
+    using Web.ViewModels.Picture;
+
+    public static class AddAndEditPictureViewModelMock
+    {
+        public static AddAndEditPictureViewModel Instance()
+        {
+            var addAndEditPictureViewModel = new AddAndEditPictureViewModel
+            {
+                Name = "Момиче с цвете",
+                Size = "40 х 60",
+                Material = "Акрил",
+                ImageAddress = "\\lib\\images\\Girl1.jpg",
+                ImageBase = "Платно",
+                CategoryId = 2,
+                Description = "Африканско момиче с цвете в косата"
+            };
+            return addAndEditPictureViewModel;
+        }
+    }
+}
