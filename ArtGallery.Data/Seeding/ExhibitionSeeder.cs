ff7a93b55bb216bf0b9f191f99e2ad82dc09be46
Oleg Anokhin <oleg_anokhin@abv.bg>
2023-08-11 08:37:03 +0300

    Fix Date for correct ordering

--- ArtGallery.Data/Seeding/ExhibitionSeeder.cs
+++ ArtGallery.Data/Seeding/ExhibitionSeeder.cs
@@ -14,8 +14,8 @@
                 Id = 1,
                 Name = "Изложба Африка",
                 ImageUrl = "\\lib\\images\\ExhibitionAfrica.jpg",
-                Start = new DateTime(2023,7,20,10,0,0),
-                End = new DateTime(2023,7,30,17,0,0),
+                Start = new DateTime(2023,9,20,10,0,0),
+                End = new DateTime(2023,9,30,17,0,0),
                 Place = "Варна, Галерията на Петя",
                 Description = "В тази изложба на тема Африка ще бъдат представени тематични картина на хора и животни."
             };
